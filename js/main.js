cb5d5d661312210485f65a2e65608defbd99e464
lauragonzalezcalvo <laura24gc@gmail.com>
2022-10-31 09:33:28 +0100

    Add querys and functions

---
+++ js/main.js
@@ -0,0 +1,12 @@
+"use strict";
+
+//query selector.Llamamos a las clases con las que vamos a trabajar
+//las razas que vamos a seleccionar
+
+const races = document.querySelector (".js-races");
+const btn = document.querySelector (".js-button");
+const result = document.querySelector (".js-result");
+
+js-races
+js-button
+js-result
